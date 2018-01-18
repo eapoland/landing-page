71286917b10df327420dc3e6d1c5c8caf61074ad
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-01-18 09:29:00 +0100

    GH-46 add script for finding oversided photos

---
+++ scripts/members-photo.js
@@ -0,0 +1,21 @@
+/* eslint-env es6 */
+
+const photoFolder = './src/images/members/',
+  { promisify } = require('util'),
+  fs = require('fs'),
+  Rx = require('rxjs/Rx'),
+  readDir = promisify(fs.readdir),
+  sizeOf = promisify(require('image-size')),
+  limit = 138;
+
+const filesArray$ = Rx.Observable.from(readDir(photoFolder)),
+  files$ = filesArray$.flatMap(array => Rx.Observable.from(array)),
+  paths$ = files$.map(fileName => photoFolder + fileName),
+  imageInfo$ = paths$.mergeMap(path => sizeOf(path), (path, fileInfo) => {
+    fileInfo.path = path;
+    return fileInfo;
+  }),
+  overSized$ = imageInfo$.filter((imageInfo) => imageInfo.height > limit || imageInfo.width > limit),
+  overSizedPaths$ = overSized$.map(imageInfo => imageInfo.path);
+
+overSizedPaths$.subscribe(path => console.log(path));
