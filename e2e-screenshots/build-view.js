30fa0f5b1895ebd648ecdcf581dc512983d3d188
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-19 16:51:14 +0000

    Deploy eapoland/landing-page to github.com/eapoland/landing-page.git:gh-pages

---
+++ e2e-screenshots/build-view.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const glob = require('glob'),
+  fs = require('fs');
+
+const files = glob.sync('*png');
+
+const getLine = file => `<a id="${file}" href="#${file}">${file}</a>:<br/><img src="${file}" />`;
+
+fs.writeFileSync(
+  'index.html',
+  '<style>img {border: 1px solid black; margin-bottom: 1em;}</style>' +
+    files.map(getLine).join('<br>'),
+  'utf8',
+);
